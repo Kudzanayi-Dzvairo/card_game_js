76c9b749eb8fa2abc34863390a48cbf9eb46260a
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-05-27 16:24:40 -0400

    card functionality

---
+++ app.js
@@ -0,0 +1,97 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('Assets/Audio/creepy.mp3');
+        this.flipSound = new Audio('Assets/Audio/flip.wav');
+        this.matchSound = new Audio('Assets/Audio/match.wav');
+        this.victorySound = new Audio('Assets/Audio/victory.wav');
+        this.gameOverSound = new Audio('Assets/Audio/gameover.wav');
+        this.bgMusic.volume = 0.1;
+        this.bgMusic.loop = true;
+    }
+
+    startMusic(){
+        this.bgMusic.play();
+    }
+    stopMusic(){
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    flip(){
+        this.flipSound.play();
+    }
+    match(){
+        this.matchSound.play();
+    }
+    victory(){
+        this.stopMusic();
+        this.victorySound.play();
+    }
+    gameOver(){
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
+
+class MixOrMatch {
+    constructor(totalTime, cards){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+
+    startGame(){
+        this.cardCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchCards = [];
+        this.busy = true;
+    }
+    flipCard(card){
+        if(this.canFlipCard){
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerHTML = this.totalClicks;
+            card.classList.add('visible');
+
+            //if statement
+        }
+
+    }
+    canFlipCard(card){
+        return true
+        // return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+    }
+}
+
+//to start game
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards)
+    
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible')
+            game.startGame()
+        })
+    })
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+        game.flipCard(card)
+        })
+    })
+    
+}
+
+//on page load
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready())
+} else {
+    ready();
+}
+
+let audioController = new AudioController();
+audioController.startMusic
